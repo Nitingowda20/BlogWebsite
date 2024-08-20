cb805cd320cb2c9b2c55c76d9cdffe7131d12f6d
Nitingowda20 <Nitingowda3448@gmail.com>
2024-08-21 02:36:38 +0530

    added total feature

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,16 +1,20 @@
 import { View, Text, Platform, StyleSheet ,FlatList} from 'react-native'
 import { StatusBar } from 'expo-status-bar'
-import { CartContext , useCart } from '../provider/cartprovider'
+import { useCart } from '../provider/cartprovider'
 import CartListItem from '../components/CartListItem'
+import Button from '../components/button'
 
 const CartScreen = () => {
-  const { items } = useCart()
+  const { items , total } = useCart()
   return (
     <View>
       <FlatList 
       data={items}
-      renderItem={({ item}) => <CartListItem cartItem={item} />} />
-      <Text style={style.text}>Cart items length : {items.length}</Text>
+      renderItem={({ item}) => <CartListItem cartItem={item} />} 
+      contentContainerStyle ={{ gap:20}} />
+
+      <Text style={style.text}>Total ${total}</Text>
+      <Button text='Checkout'/>
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
 
     </View>
@@ -19,6 +23,8 @@ const CartScreen = () => {
  
 const style =StyleSheet.create({
   text:{
+    marginTop:20,
+    marginBottom: 5,
     fontSize : 30,
     color:"white"
   }
